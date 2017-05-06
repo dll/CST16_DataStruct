7d4fb70c13a477542bb38a46ea8a4d9055a3f259
czldl <czldl@tom.com>
2017-05-06 22:21:24 +0800

    链表合并：非递减Non-Decreasing！

--- FQA/数据结构-线性表归并.cpp
+++ FQA/数据结构-线性表归并.cpp
@@ -1,3 +1,5 @@
+//Fan student Question!
+
 # include<stdio.h>
 # include<stdlib.h>
 typedef int ElemType;
@@ -8,30 +10,37 @@ typedef struct LNode{
 void CreatList(LinkList &L,int n)
 {
 	int i;
-	LinkList p;
+	LinkList p,q;
 	L=(LinkList)malloc(sizeof(LNode));
+	L->data=0;
 	L->next=NULL;
+	q=L;
 	for(i=n;i>0;--i){
 		p=(LinkList)malloc(sizeof(LNode));
 		scanf("%d",&p->data);
-		
-		p->next=L->next;
-		L->next=p;
+		if(q->data>p->data){
+			printf("LinkList is Non-Decreasing!!");
+			exit(0);
+		}
+		p->next=NULL;
+		q->next=p;
+		q=p;
 	}
 	
 }
 
 void printf1(LinkList L){
-LinkList p;
+	LinkList p;
 	p=L->next;
 	while(p){
-		printf("%d ",p->data);
+		printf("%d -->",p->data);
 		p=p->next;
 	}
+	printf("\n");
 }
 
 void MergeList(LinkList &La,LinkList &Lb,LinkList  &Lc){
-LinkList pa,pb,pc;
+	LinkList pa,pb,pc;
 	pa=La->next;
     pb=Lb->next;
 	Lc=pc=La;
@@ -52,9 +61,8 @@ LinkList pa,pb,pc;
 
 	free(Lb);
 	Lb=NULL;
-	printf1(Lc);
 }
-void main(){
+int main(){
 	int s,m;
 	LinkList Lc,La,Lb;
 	printf("Enter the number of La:\n");
@@ -65,8 +73,23 @@ void main(){
 	scanf("%d",&m);
 	CreatList(Lb,m);
 	printf1(Lb);
+	printf("After Merge List:\n");
 	MergeList(La,Lb,Lc);
-	//printf1(Lc);
-
+	printf1(Lc);
+	
+	return 0;
 }
 
+/*
+Enter the number of La:
+3
+2 3 33
+2 -->3 -->33 -->
+Enter the number of Lb:
+2
+1 22
+1 -->22 -->
+After Merge List:
+1 -->2 -->3 -->22 -->33 -->
+
+*/
